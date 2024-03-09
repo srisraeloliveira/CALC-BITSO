642e18da2ff77a8a055f073e1e105275823fbd76
srisraeloliveira <isra.dias.oliveira@gmail.com>
2024-03-09 19:11:12 -0300

    feat: fix

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 // Adiciona eventos de escuta para os campos de entrada
-document.getElementById('cambio-oficial').addEventListener('input', calcularCambioRevenda);
-document.getElementById('percentual-lucro').addEventListener('input', calcularCambioRevenda);
-document.getElementById('spread-medio').addEventListener('input', calcular);
-document.getElementById('valor').addEventListener('input', function() {
-    alternarModoOperacao();
-    calcularCambioRevenda();
-    calcular();
-});
+document.getElementById('cambio-oficial').addEventListener('input', atualizarCalculos);
+document.getElementById('percentual-lucro').addEventListener('input', atualizarCalculos);
+document.getElementById('spread-medio').addEventListener('input', atualizarCalculos);
+document.getElementById('valor').addEventListener('input', atualizarCalculos);
 document.getElementById('tipo-operacao').addEventListener('change', alternarModoOperacao);
-document.getElementById('taxa-efetivo').addEventListener('input', calcularCambioRevenda);
-document.getElementById('desconto').addEventListener('input', function() {
+document.getElementById('taxa-efetivo').addEventListener('input', atualizarCalculos);
+document.getElementById('desconto').addEventListener('input', atualizarCalculos);
+
+// Adiciona evento de clique ao botão "Limpar"
+document.getElementById('limpar-btn').addEventListener('click', function() {
+    limparCampos();
+    // Recarrega a página
+    location.reload();
+});
+
+// Função para atualizar os cálculos
+function atualizarCalculos() {
     calcularCambioRevenda();
     calcular();
-});
+    alternarDesconto();
+}
 
 // Função para calcular o câmbio de revenda
 function calcularCambioRevenda() {
     var cambioOficial = parseFloat(document.getElementById('cambio-oficial').value);
     var percentualLucro = parseFloat(document.getElementById('percentual-lucro').value) / 100; // Convertendo para decimal
-    var spreadMedio = parseFloat(document.getElementById('spread-medio').value) / 100; // Spread médio em decimal
     var tipoOperacao = document.getElementById('tipo-operacao').value;
-    var valor = parseFloat(document.getElementById('valor').value);
     var desconto = parseFloat(document.getElementById('desconto').value)/ 100;
 
     var cambioRevenda, taxaEfetivo;
@@ -36,7 +41,7 @@ function calcularCambioRevenda() {
     if (isNaN(cambioRevenda)) {
         document.getElementById('cambio-revenda').textContent = "---";
     } else {
-        document.getElementById('cambio-revenda').textContent = cambioRevenda.toFixed(2);
+        document.getElementById('cambio-revenda').textContent =  cambioRevenda.toFixed(2);
     }
     
     document.getElementById('cambio-revenda-container').style.display = 'block';
@@ -46,7 +51,6 @@ function calcularCambioRevenda() {
 function calcular() {
     var valor = parseFloat(document.getElementById('valor').value);
     var spreadMedio = parseFloat(document.getElementById('spread-medio').value) / 100; // Spread médio em decimal
-    var tipoOperacao = document.getElementById('tipo-operacao').value;
     var cambioRevenda = parseFloat(document.getElementById('cambio-revenda').textContent);
 
     var valorTotal = valor * parseFloat(document.getElementById('cambio-oficial').value);
@@ -57,28 +61,37 @@ function calcular() {
 
     // Verifica se algum valor calculado é NaN e substitui por '---'
     if (isNaN(valorTotal) || isNaN(lucro) || isNaN(valorEnviar)) {
-        resultadoDiv.innerHTML = "<strong>Valor Total:</strong> ---<br>";
-        resultadoDiv.innerHTML += "<strong>Lucro:</strong> ---<br>";
-        resultadoDiv.innerHTML += "<strong>A Enviar:</strong> ---";
+        resultadoDiv.innerHTML = "<strong>Valor Total - AR$:</strong> ---<br>";
+        resultadoDiv.innerHTML += "<strong>A Enviar - AR$:</strong> ---<br>";
+        resultadoDiv.innerHTML += "<strong>Lucro - R$:</strong> ---<br>";
     } else {
-        resultadoDiv.innerHTML = "<strong>Valor Total:</strong> $" + valorTotal.toFixed(2) + "<br>";
-        resultadoDiv.innerHTML += "<strong>Lucro:</strong> $" + lucro.toFixed(2) + "<br>";
-        resultadoDiv.innerHTML += "<strong>A Enviar:</strong> $" + valorEnviar.toFixed(2);
+        resultadoDiv.innerHTML = "<strong>Valor Total </strong> AR$ " + valorTotal.toFixed(2) + "<br>";
+        resultadoDiv.innerHTML += "<strong>A Enviar </strong> AR$ " + valorEnviar.toFixed(2)+ "<br>";
+        resultadoDiv.innerHTML += "<strong>Lucro </strong> R$ " + lucro.toFixed(2) + "<br>";
     }
 }
 
 // Função para limpar os campos
 function limparCampos() {
     document.getElementById('cambio-oficial').value = "";
-    document.getElementById('percentual-lucro').value = "";
-    document.getElementById('spread-medio').value = "";
+    document.getElementById('percentual-lucro').value = "2.5";
+    document.getElementById('spread-medio').value = "0.98";
     document.getElementById('valor').value = "";
     document.getElementById('resultado').innerHTML = "";
-    document.getElementById('cambio-revenda').textContent = "---";
-    document.getElementById('cambio-revenda-container').style.display = 'none';
     document.getElementById('taxa-efetivo').value = "";
     document.getElementById('desconto').value = "";
     document.getElementById('desconto-container').style.display = 'none';
+}
+
+// Função para alternar a exibição do campo de desconto
+function alternarDesconto() {
+    var valor = parseFloat(document.getElementById('valor').value);
+    var descontoContainer = document.getElementById('desconto-container');
+    if (valor > 500) {
+        descontoContainer.style.display = 'block';
+    } else {
+        descontoContainer.style.display = 'none';
+    }
 }
 
 // Função para alternar o modo de operação
